3ec9957cd9ef12463d6b3a781278aaa00ec0e1d6
Chem O'Dun <chemodun@gmail.com>
2025-07-03 12:22:36 +0300

    refactor(languageFiles): move language file handling logic to languageFiles.ts and streamline loading process

--- extensions/scripts/src/extension.ts
+++ extensions/scripts/src/extension.ts
@@ -12,6 +12,7 @@ import { ScriptProperties } from './scripts/scriptProperties';
 import { getDocumentScriptType, scriptsMetadata, aiScriptId, mdScriptId, scriptNodes, scriptsMetadataSet, scriptsMetadataClearAll } from './scripts/scriptsMetadata';
 import { VariableTracker } from './scripts/scriptVariables';
 import { ScriptCompletion } from './scripts/scriptCompletion';
+import { findLanguageText, languageDataClearAll, loadLanguageFiles } from './languageFiles/languageFiles';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,7 +20,6 @@ let isDebugEnabled = false;
 let rootpath: string;
 let extensionsFolder: string;
 let forcedCompletion: boolean = false;
-let languageData: Map<string, Map<string, string>> = new Map();
 let xsdReference: XsdReference;
 let scriptProperties: ScriptProperties;
 
@@ -296,159 +296,6 @@ function trackScriptDocument(document: vscode.TextDocument, update: boolean = fa
 
 let scriptCompletionProvider : ScriptCompletion;
 
-// load and parse language files
-function loadLanguageFiles(basePath: string, extensionsFolder: string): Promise<void> {
-  const config = vscode.workspace.getConfiguration('x4CodeComplete');
-  const preferredLanguage: string = config.get('languageNumber') || '44';
-  const limitLanguage: boolean = config.get('limitLanguageOutput') || false;
-  languageData = new Map();
-  logger.info('Loading Language Files.');
-  return new Promise((resolve, reject) => {
-    try {
-      const tDirectories: string[] = [];
-      let pendingFiles = 0; // Counter to track pending file parsing operations
-      let countProcessed = 0; // Counter to track processed files
-
-      // Collect all valid 't' directories
-      const rootTPath = path.join(basePath, 't');
-      if (fs.existsSync(rootTPath) && fs.statSync(rootTPath).isDirectory()) {
-        tDirectories.push(rootTPath);
-      }
-      // Check 't' directories under languageFilesFolder subdirectories
-      if (fs.existsSync(extensionsFolder) && fs.statSync(extensionsFolder).isDirectory()) {
-        const subdirectories = fs
-          .readdirSync(extensionsFolder, { withFileTypes: true })
-          .filter((item) => item.isDirectory())
-          .map((item) => item.name);
-
-        for (const subdir of subdirectories) {
-          const tPath = path.join(extensionsFolder, subdir, 't');
-          if (fs.existsSync(tPath) && fs.statSync(tPath).isDirectory()) {
-            tDirectories.push(tPath);
-          }
-        }
-      }
-
-      // Process all found 't' directories
-      for (const tDir of tDirectories) {
-        const files = fs.readdirSync(tDir).filter((file) => file.startsWith('0001') && file.endsWith('.xml'));
-
-        for (const file of files) {
-          const languageId = getLanguageIdFromFileName(file);
-          if (limitLanguage && languageId !== preferredLanguage && languageId !== '*' && languageId !== '44') {
-            // always show 0001.xml and 0001-0044.xml (any language and english, to assist with creating translations)
-            continue;
-          }
-          const filePath = path.join(tDir, file);
-          pendingFiles++; // Increment the counter for each file being processed
-          try {
-            parseLanguageFile(filePath, () => {
-              pendingFiles--; // Decrement the counter when a file is processed
-              countProcessed++; // Increment the counter for processed files
-              if (pendingFiles === 0) {
-                logger.info(`Loaded ${countProcessed} language files from ${tDirectories.length} 't' directories.`);
-                resolve(); // Resolve the promise when all files are processed
-              }
-            });
-          } catch (fileError) {
-            logger.info(`Error reading ${file} in ${tDir}: ${fileError}`);
-            pendingFiles--; // Decrement the counter even if there's an error
-            if (pendingFiles === 0) {
-              resolve(); // Resolve the promise when all files are processed
-            }
-          }
-        }
-      }
-
-      if (pendingFiles === 0) {
-        resolve(); // Resolve immediately if no files are found
-      }
-    } catch (error) {
-      logger.info(`Error loading language files: ${error}`);
-      reject(error); // Reject the promise if there's an error
-    }
-  });
-}
-
-function getLanguageIdFromFileName(fileName: string): string {
-  const match = fileName.match(/0001-[lL]?(\d+).xml/);
-  return match && match[1] ? match[1].replace(/^0+/, '') : '*';
-}
-
-function parseLanguageFile(filePath: string, onComplete: () => void) {
-  const parser = sax.createStream(true); // Create a streaming parser in strict mode
-  let currentPageId: string | null = null;
-  let currentTextId: string | null = null;
-  const fileName: string = path.basename(filePath);
-  const languageId: string = getLanguageIdFromFileName(fileName);
-
-  parser.on('opentag', (node) => {
-    if (node.name === 'page' && node.attributes.id) {
-      currentPageId = node.attributes.id as string;
-    } else if (node.name === 't' && currentPageId && node.attributes.id) {
-      currentTextId = node.attributes.id as string;
-    }
-  });
-
-  parser.on('text', (text) => {
-    if (currentPageId && currentTextId) {
-      const key = `${currentPageId}:${currentTextId}`;
-      const textData: Map<string, string> = languageData.get(key) || new Map<string, string>();
-      textData.set(languageId, text.trim());
-      languageData.set(key, textData);
-    }
-  });
-
-  parser.on('closetag', (nodeName) => {
-    if (nodeName === 't') {
-      currentTextId = null; // Reset text ID after closing the tag
-    } else if (nodeName === 'page') {
-      currentPageId = null; // Reset page ID after closing the tag
-    }
-  });
-
-  parser.on('end', () => {
-    onComplete(); // Notify that this file has been fully processed
-  });
-
-  parser.on('error', (err) => {
-    logger.info(`Error parsing standard language file ${filePath}: ${err.message}`);
-    onComplete(); // Notify even if there's an error
-  });
-
-  fs.createReadStream(filePath).pipe(parser);
-}
-
-function findLanguageText(pageId: string, textId: string): string {
-  const config = vscode.workspace.getConfiguration('x4CodeComplete');
-  let preferredLanguage: string = config.get('languageNumber') || '44';
-  const limitLanguage: boolean = config.get('limitLanguageOutput') || false;
-
-  const textData: Map<string, string> = languageData.get(`${pageId}:${textId}`);
-  let result: string = '';
-  if (textData) {
-    const textDataKeys = Array.from(textData.keys()).sort((a, b) =>
-      a === preferredLanguage
-        ? -1
-        : b === preferredLanguage
-          ? 1
-          : (a === '*' ? 0 : parseInt(a)) - (b === '*' ? 0 : parseInt(b))
-    );
-    if (limitLanguage && !textData.has(preferredLanguage)) {
-      if (textData.has('*')) {
-        preferredLanguage = '*';
-      } else if (textData.has('44')) {
-        preferredLanguage = '44';
-      }
-    }
-    for (const language of textDataKeys) {
-      if (!limitLanguage || language == preferredLanguage) {
-        result += (result == '' ? '' : `\n\n`) + `${language}: ${textData.get(language)}`;
-      }
-    }
-  }
-  return result;
-}
 
 
 export function activate(context: vscode.ExtensionContext) {
@@ -981,10 +828,8 @@ export function deactivate() {
       // Script completion provider will be garbage collected
     }
 
-    // Clear language data
-    if (languageData) {
-      languageData.clear();
-    }
+
+    languageDataClearAll();
 
     // Clear scripts metadata
     if (scriptsMetadata) {
