b732d10102c09fb24c244be39a1e2c9940bd8106
Chem O'Dun <chemodun@gmail.com>
2025-07-24 19:44:15 +0300

    build(scripts): update ESLint and TypeScript configuration for improved file handling and exclusions

--- extensions/scripts/eslint.config.mjs
+++ extensions/scripts/eslint.config.mjs
@@ -4,8 +4,8 @@ import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { ignores: ['node_modules/**', 'dist/**', 'out/**'] },
+  { files: ['src/*.{js,mjs,cjs,ts}'] },
+  { ignores: ['node_modules/**', 'dist/**', 'out/**', '.vscode-test/**', 'temp/**'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
