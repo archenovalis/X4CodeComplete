188ffaa1a461ba1e1e7f8dcc0523293de7e07dee
Chem O'Dun <chemodun@gmail.com>
2025-08-22 14:35:14 +0300

    feat(shared): add lValueTypes and updateLValueTypes function
      +added lValueTypes constant (initializes with 'lvalueexpression')
      +added updateLValueTypes function (updates lValueTypes with new types)

--- extensions/scripts/src/extension/shared.ts
+++ extensions/scripts/src/extension/shared.ts
@@ -1,2 +1,13 @@
 import * as vscode from 'vscode';
 export const diagnosticCollection: vscode.DiagnosticCollection = vscode.languages.createDiagnosticCollection('x4CodeComplete');
+export const lValueTypes: string[] = ['lvalueexpression'];
+
+export function updateLValueTypes(newTypes: string[]) {
+  if (Array.isArray(newTypes) && newTypes.length > 0) {
+    for (const type of newTypes) {
+      if (!lValueTypes.includes(type)) {
+        lValueTypes.push(type);
+      }
+    }
+  }
+}
